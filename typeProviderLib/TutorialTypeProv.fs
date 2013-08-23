bf7c8924c2e910c7cf11c3cb764a134962eddc77
martinleinberger <martin.leinberger@googlemail.com>
2013-08-23 11:09:40 +0200

    initial commit

---
+++ typeProviderLib/TutorialTypeProv.fs
@@ -0,0 +1,82 @@
+﻿module TutorialTypeProv
+
+open System.Reflection
+open Samples.FSharp.ProvidedTypes
+open FSharp.Data.Json.Extensions
+
+open Microsoft.FSharp.Core.CompilerServices
+open Microsoft.FSharp.Quotations
+open System.Net
+open System.IO
+open FSharp.Data.Json
+
+[<TypeProvider>]
+type SampleTypeProvider(config: TypeProviderConfig) as this = class
+    inherit TypeProviderForNamespaces()
+
+    let namespaceName = "HelloWorldProvider"
+    let thisAssembly = Assembly.GetExecutingAssembly()
+
+    let makeOneProvidedType(n: int) =
+        let t = ProvidedTypeDefinition(thisAssembly, namespaceName, "Type" + string n, baseType = Some typeof<obj>)
+
+        t.AddXmlDocDelayed( fun() -> sprintf "This provided type %s" ("Type" + string n) )
+
+        let staticProp = ProvidedProperty(propertyName = "StaticProperty",
+                                          propertyType = typeof<string>,
+                                          IsStatic = true,
+                                          GetterCode = (fun args -> <@@ "Hello!" @@>))
+                                          
+        staticProp.AddXmlDocDelayed(fun () -> "This is a static property")
+        
+        t.AddMember staticProp 
+        let ctor = ProvidedConstructor(parameters = [], InvokeCode = (fun args -> <@@ "The object data" :> obj @@>))
+        ctor.AddXmlDocDelayed(fun () -> "This is a constructor")
+
+        t.AddMember ctor
+
+        let ctor2 = ProvidedConstructor(parameters = [ ProvidedParameter("data", typeof<string>) ], InvokeCode= (fun args -> <@@ (%%(args.[0]) : string) :> obj @@>))
+        ctor2.AddXmlDocDelayed(fun () -> "This is a constructor")
+        t.AddMember ctor2
+
+        let instanceProp = ProvidedProperty(propertyName="InstanceProperty", propertyType = typeof<int>, GetterCode = (fun args -> <@@ ((%%(args.[0]) : obj) :?> string).Length @@>))
+        instanceProp.AddXmlDocDelayed( fun() -> "This is an instance property" )
+        t.AddMember instanceProp
+
+        let instanceMeth = ProvidedMethod(methodName = "InstanceMethod", parameters = [ProvidedParameter("x", typeof<int>)], returnType = typeof<char>,
+                                          InvokeCode = (fun args -> <@@ ((%%(args.[0]) : obj) :?> string).Chars(%%(args.[1]) : int) @@>))
+        instanceMeth.AddXmlDocDelayed( fun() -> "This is an instance method" )
+        t.AddMember instanceMeth
+
+        t.AddMembersDelayed(fun () -> 
+            let nestedType = ProvidedTypeDefinition("NestedType",
+                                                    Some typeof<obj>)
+
+            // Each nested type contains 100 static properties, provided on-demand.
+            // The static properties have constant values.
+            nestedType.AddMembersDelayed (fun () -> 
+                let staticPropsInNestedType = 
+                    [ for i in 1 .. 100 do
+                        let valueOfTheProperty = "I am string "  + string i
+
+                        let p = ProvidedProperty(propertyName = "StaticProperty" + string i, 
+                                                 propertyType = typeof<string>, 
+                                                 IsStatic=true,
+                                                 GetterCode= (fun args -> <@@ valueOfTheProperty @@>))
+
+                        p.AddXmlDocDelayed(fun () -> 
+                               sprintf "This is StaticProperty%d on NestedType" i)
+
+                        yield p ]
+                
+                staticPropsInNestedType)
+
+            [nestedType])
+
+
+        t
+
+    let types = [ for i in 1 .. 10 -> makeOneProvidedType i ]
+
+    do this.AddNamespace(namespaceName, types)
+end
---
+++ typeProviderLib/TypeProv.fs
@@ -0,0 +1,208 @@
+﻿namespace HelloWorldProvider
+
+open System.Reflection
+open Samples.FSharp.ProvidedTypes
+open FSharp.Data.Json.Extensions
+
+open Microsoft.FSharp.Core.CompilerServices
+open Microsoft.FSharp.Quotations
+open System.Net
+open System.IO
+open FSharp.Data.Json
+
+[<AutoOpen>]
+module HelperModule =
+    let load url = 
+        let req = WebRequest.Create(url : string)
+        JsonValue.Load(req.GetResponse().GetResponseStream())
+
+    let loadFromFs path =
+        let reader = new StreamReader(path:string)
+        JsonValue.Load(reader)
+
+    type ExplorerData(data:JsonValue) as x = class
+        let mutable containedObjects: Map<string, ExplorerData> = Map.empty
+        let mutable stringArrays: Map<string, seq<string>> = Map.empty
+        let mutable objectArrays: Map<string, seq<ExplorerData>> = Map.empty
+
+        do
+            let objects =
+                data.Properties |> Seq.filter( fun (key, value) ->
+                    //keine ahnung obs ne bessere Möglichkeit gibt zu checken ob value ein json object ist
+                    try
+                        value.Properties |> Seq.iter( fun args -> () )
+                        true
+                    with
+                        _ -> false
+                )
+                |> Seq.map( fun (key, value) ->
+                    let value' = ExplorerData(value)
+                    key, value'
+                )
+
+            let strLists = 
+                data.Properties |> Seq.filter( fun (key, value) ->
+                    //test obs ein string array ist
+                    try
+                        value.Item(0).AsString() |> ignore
+                        true    
+                    with
+                        _ -> false                 
+                )
+                |> Seq.map( fun (key, value) ->
+                    let value' = value.AsArray() |> Seq.map( fun x -> x.AsString() )
+                    key, value'
+                )
+
+            let objLists = 
+                data.Properties |> Seq.filter ( fun (key, value) ->
+                    try
+                        value.Item(0).Properties |> ignore
+                        true
+                    with
+                        _ -> false
+                )
+                |> Seq.map( fun (key, value) ->
+                    let value' = value.AsArray() |> Seq.map( fun x -> new ExplorerData(x) )
+                    key, value'
+                )
+            
+            x.ContainedObjects <- Map.ofSeq objects
+            x.StringArrays <- Map.ofSeq strLists
+            x.ObjectArrays <- Map.ofSeq objLists
+
+        new (url:string) =
+            let d = load url
+            ExplorerData(d)
+
+        member private x.ContainedObjects
+            with get() = containedObjects
+            and set(v) = containedObjects <- v
+
+        member private x.StringArrays
+            with get() = stringArrays
+            and set(v) = stringArrays <- v
+
+        member private x.ObjectArrays
+            with get() = objectArrays
+            and set(v) = objectArrays <- v
+
+        member x.Data = data
+        member x.GetString(propertyName:string) =
+            x.Data.GetProperty(propertyName).AsString()
+        
+        member x.GetObject(pName:string) = 
+            x.ContainedObjects.Item(pName)
+
+        member x.GetStringArray(pName:string) = 
+            x.StringArrays.Item(pName)
+
+        member x.GetObjectArray(pName:string) = 
+            x.ObjectArrays.Item(pName)
+    end
+
+    //---------- helper methods for property creation ----------\\
+    let makeStringProperty(propertyName, xmlDoc:string) = 
+        let p = ProvidedProperty(propertyName = propertyName, propertyType = typeof<string>, IsStatic = false, GetterCode = (fun [ me ] ->
+            <@@ ((%%me:obj):?>ExplorerData).GetString(propertyName) @@>
+        ))
+        p.AddXmlDoc xmlDoc
+        p
+
+    let makeObjectProperty(propertyName, xmlDoc, typeDefinition) = 
+        let p = ProvidedProperty(propertyName=propertyName, propertyType = typeDefinition, GetterCode = (fun [me] ->
+            <@@ ((%%me:obj):?>ExplorerData).GetObject(propertyName) @@>
+        ))
+        p.AddXmlDoc xmlDoc       
+        p
+
+    let makeStringArrayProperty(propertyName, xmlDoc) = 
+        let p = ProvidedProperty(propertyName=propertyName, propertyType = typedefof<seq<_>>.MakeGenericType(typeof<string>), GetterCode = ( fun [me] ->
+            <@@  ((%%me:obj):?>ExplorerData).GetStringArray(propertyName) @@>
+        ))
+        p.AddXmlDoc xmlDoc
+        p
+
+    let makeObjectArrayProperty(propertyName, xmlDoc, typeDefinition:ProvidedTypeDefinition) =
+        let p = ProvidedProperty(propertyName=propertyName, propertyType = typedefof<seq<_>>.MakeGenericType(typeDefinition), GetterCode = ( fun [me] ->
+            <@@ ((%%me:obj):?>ExplorerData).GetObjectArray(propertyName) @@>
+        ))
+        p.AddXmlDoc xmlDoc
+        p
+
+    //----------   actual parsing ----------\\
+    let rec parse(key:string, value:JsonValue, initialCall:bool) =
+        let typeDefinition =
+            if initialCall then
+                ProvidedTypeDefinition(Assembly.GetExecutingAssembly(), "OneOhOne", key, baseType = Some typeof<obj>)
+            else
+                ProvidedTypeDefinition(key, baseType = Some typeof<obj>)
+
+        value.GetProperty("properties").Properties |> Seq.iter( fun (key', value') ->
+            let valType = value'.GetProperty("type").AsString()
+            match valType with
+                | "string" ->
+                    let doc = "<summary>"+value'.GetProperty("description").AsString()+"</summary>"
+                    let p = makeStringProperty(key', doc)
+                    typeDefinition.AddMember p
+                | "object" ->
+                    //first add newly created type, then add actual property
+                    let t' = parse(key', value', false)
+                    typeDefinition.AddMember t'
+
+                    let doc = "<summary>"+value'.GetProperty("description").AsString()+"</summary>"
+                    let p = makeObjectProperty(key', doc, t') 
+                    typeDefinition.AddMember p
+                | "array" ->
+                    let itemType = value'.GetProperty("items").GetProperty("type").AsString()
+                    match itemType with
+                        | "string" ->
+                            let doc = "<summary>" + value'.GetProperty("description").AsString() + "</summary>"
+                            let p = makeStringArrayProperty(key', doc)
+                            typeDefinition.AddMember p
+                        | "object" ->
+                            let t' = parse(key'+"Item", value'.GetProperty("items"), false)
+                            typeDefinition.AddMember t'
+                            
+                            let doc = "<summary>" + value'.GetProperty("description").AsString() + "</summary>"
+                            let p = makeObjectArrayProperty(key', doc, t')
+                            typeDefinition.AddMember p
+                        | _ ->
+                            let errMsg = "unkown type in array " + key'
+                            failwith errMsg
+                | _ ->
+                    let errMsg = "unkown type in object " + key
+                    failwith errMsg
+        ) 
+
+        typeDefinition:ProvidedTypeDefinition
+        
+
+[<TypeProvider>]
+type OneOhOneProvider(config: TypeProviderConfig) as this = class
+    inherit TypeProviderForNamespaces()
+
+    let namespaceName = "OneOhOne"
+    //let fileSchema = loadFromFs "C:/Users/Martin/Desktop/101repo_file.json"
+    let fileSchema = load "https://raw.github.com/101companies/101worker/master/schemas/101repo_file.json"
+
+    let makeFileType = 
+        let t = parse("File", fileSchema, true)
+        let ctor = ProvidedConstructor(
+                    parameters = [ ProvidedParameter("targetUrl", parameterType=typeof<string>) ],
+                    InvokeCode = (fun [ targetUrl ] -> <@@ new ExplorerData(%%targetUrl:string) @@>)
+        )
+        ctor.AddXmlDoc("<summary>Constructor for a file</summary>")
+        t.AddMember ctor
+
+        t
+
+    let types = [ makeFileType ]
+
+    do
+        this.AddNamespace(namespaceName, types)
+
+end
+
+[<assembly:TypeProviderAssembly>]
+do()
